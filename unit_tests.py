c1576a7b2fffd9c5ba320a312be4439e818a3d78
madtyn <madtyn@gmail.com>
2020-01-02 11:07:41 +0100

    Adds tests for process_student() and is_valid()

--- unit_tests.py
+++ unit_tests.py
@@ -1,9 +1,44 @@
 import unittest
 
+from cris import process_student, is_valid
+from models import CommonInfo, Receipt, StudentMonth
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    info = CommonInfo('Killstobal Boedo Buño', '21548796F', '2019/20', 'SONICREQUES')
+
+    def test_is_valid(self):
+        sm = StudentMonth('X', 'X', 'N')
+        assert not is_valid(sm), 'Failed to validate XXN'
+        sm = StudentMonth(12, 12, 'S')
+        assert not is_valid(sm), 'Failed to validate an already processed month'
+        sm = StudentMonth(15, 11, 'N')
+        assert is_valid(sm), 'Failed to validate a correct month'
+
+    def test_process_student(self):
+        row = ['María Calviño Seixo',
+               '15', '17', 'S',
+               '30', '17', 'N',
+               '45', '17', 'N',
+               'X', 'X', 'N',
+               'X', 'X', 'N',
+               '30', '12', 'S',
+               '45', '12', 'N',
+               'X', 'X', 'N',
+               'X', 'X', 'N',
+               '60', 'X', 'N',
+               '15', 'X', 'S',
+               'X', 'X', 'N',
+               ]
+        result = process_student(self.info, row)
+        assert type(result) == list and all(type(x) is Receipt for x in result)
+        ref_row = []
+        for i in range(1, 10):
+            sm = StudentMonth(row[i:i + 3])
+            if is_valid():
+                ref_row.append(sm)
+
+        assert len(result) == len(ref_row)
 
 
 if __name__ == '__main__':
