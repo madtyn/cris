050e51d75495a5d4545b84fe6761a35e81b9d2a9
madtyn <madtyn@gmail.com>
2020-01-03 15:20:29 +0100

    Adds new dialogs, writes a txt output file, writes a new excel, adds tip of the day feature

--- cris.py
+++ cris.py
@@ -7,9 +7,15 @@ SCRIPT FILE TO BE EXECUTED FROM THE TEST SCRIPT
 Requirements:
 pip3 install pyexcel-ods3
 """
+import datetime
 import os.path
 import sys
-from pyexcel_ods3 import get_data
+import random
+from collections import OrderedDict
+from tkinter import filedialog as fd, Tk, messagebox
+import os
+
+from pyexcel_ods3 import get_data, save_data
 
 from indexes import *
 from messages import *
@@ -20,12 +26,24 @@ test = False
 
 
 def extract_main_header(excel_data, cell_idx, msg):
+    """
+    Extracts one field from the main header in the ods
+    :param excel_data: the data variable
+    :param cell_idx: the cell index
+    :param msg: the msg for the user
+    :return: the extracted data from cell
+    """
     result = excel_data[cell_idx][FIRST_COL_IDX]
     print(msg, result)
     return result
 
 
 def extract_common_info(excel_data):
+    """
+    Extracts the fields from the main header
+    :param excel_data: the data variable
+    :return: the header info in an object
+    """
     teacher = extract_main_header(excel_data, TEACHER_NAME_ROW_IDX, TEACHER_DETECTED_MSG)
     nif = extract_main_header(excel_data, NIF_TEACHER_ROW_IDX, NIF_DETECTED_MSG)
     school_year = extract_main_header(excel_data, SCHOOL_YEAR_ROW_IDX, SCHOOL_YEAR_DETECTED_MSG)
@@ -35,11 +53,20 @@ def extract_common_info(excel_data):
 
 
 def process_group(row):
+    """
+    Process a group title line
+    :param row: the excel row as a list
+    """
     group = row[FIRST_COL_IDX]
     print(PROCESSING_GROUP_MSG, group)
 
 
 def is_valid(sm):
+    """
+    Determines if a StudentMonth tuple is valid for processing
+    :param sm: the StudentMonth object
+    :return: True if it can be processed, False otherwise
+    """
     if 'S' in sm.processed.strip().upper():
         return False
     if type(sm.quota) is not int:
@@ -50,22 +77,61 @@ def is_valid(sm):
     return True
 
 
-def process_student(info, row):
+def process_student(info, row, output_file):
+    """
+    Process a student row, creating a Receipt and writing it to file
+    :param info: the common info for all student receipts
+    :param row: the excel row as a list
+    :param output_file: the file to write to
+    :return: all the receipts as a list variable
+    """
+    result_list = []
     student = row[FIRST_COL_IDX]
     print(PROCESSING_STUDENT_MSG, student)
 
-    result = []
     for m in Months:
         try:
             sm = m.get_student_month(row)
-            if not is_valid(sm):
-                continue
-            r = Receipt(common_info, student, sm)
-            result.append(r)
+            if is_valid(sm):
+                r = Receipt(info, student, sm).template()
+                output_file.write(r)
+                row[m.processed_idx] = 'S'
+                result_list.append(r)
         except IndexError:
             continue
 
-    return result
+    return result_list
+
+
+def ask_input(test):
+    """
+    Asks for input file
+    :param test: True if test mode, False otherwise
+    :return: the filename for the input file
+    """
+    if test:
+        print(ASK_INPUT, end=SEP)
+        input_filename = input().rstrip('\n')
+    else:
+        input_filetypes = (("Openoffice calc files", "*.ods"),)
+        input_filename = fd.askopenfilename(initialdir=os.getcwd(), filetypes=input_filetypes)
+    return input_filename
+
+
+def ask_output(test):
+    """
+    Asks for output file
+    :param test: True if test mode, False otherwise
+    :return: the file-like object for the output file
+    """
+    if test:
+        print(ASK_OUTPUT, end=SEP)
+        output_filename = input().rstrip('\n')
+        output_file = open(output_filename, 'w', encoding='utf-8')
+    else:
+        output_filetypes = (("Archivo de recibos CRIS", "*.txt"),)
+        output_file = fd.asksaveasfile(initialdir=os.getcwd(), filetypes=output_filetypes)
+    return output_file
 
 
 if __name__ == '__main__':
@@ -74,18 +140,19 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         SEP = '\n'
         test = True
+    else:
+        root = Tk()
+        root.withdraw()
 
-    print(ASK_INPUT, end=SEP)
-    input_file = input().rstrip('\n')
-    print(CHECK_INPUT_PATH)
+    input_file = ask_input(test)
+    print(CHECK_FILE_PATH)
     print(input_file)
 
-    print(ASK_OUTPUT, end=SEP)
-    output_file = input().rstrip('\n')
-    print(CHECK_INPUT_PATH)
-    print(output_file)
-
     if os.path.exists(input_file):
+        output_file = ask_output(test)
+        print(CHECK_FILE_PATH)
+        print(output_file.name)
+
         print(DETECTED_FILE_MSG)
         print(READING_FILE_MSG)
         data = get_data(input_file)['CONTABILIDAD']
@@ -103,17 +170,27 @@ if __name__ == '__main__':
 
             current_row += 1
 
-            list_receipts = []
             for _ in range(STUDENTS_PER_GROUP):
                 if len(data[current_row]):
-                    list_receipts.extend(process_student(common_info, data[current_row]))
+                    process_student(common_info, data[current_row], output_file)
                 elif test:
-                    print(PROCESSING_STUDENT_MSG, '-')  # For achieving in the test to read a \n line
+                    # For the test to read a \n line
+                    print(PROCESSING_STUDENT_MSG, '-')
                 current_row += 1
 
-            for r in list_receipts:
-                print(r.template())
+        output_file.close()
+        output_data = OrderedDict()
+        output_data.update({'CONTABILIDAD': data})
 
+        d = datetime.datetime.today()
+        new_excel_path = os.path.dirname(input_file)
+        new_excel_filename = f'{d.strftime("%Y_%m_%d_%Hh_%Mm")}_{os.path.basename(input_file)}'
+        new_excel_name = '/'.join([new_excel_path, new_excel_filename])
+        save_data(new_excel_name, output_data)
+
+        if not test:
+            msg = 'Fin de proceso. Tip del día:\n' + random.choice(END_MESSAGES)
+            messagebox.showinfo('De nada, colega', msg)
         print(SUCCESS_MSG)
     else:
         os._exit(1)
--- messages.py
+++ messages.py
@@ -1,7 +1,11 @@
+"""
+File for string messages and constants
+"""
+
 RUNNING_MSG = 'CRIS se está ejecutando...'
 ASK_INPUT = 'Introduzca la ruta y nombre del fichero de entrada (extensión .ods): '
 ASK_OUTPUT = 'Introduzca la ruta y nombre del fichero de salida (extensión .pdf): '
-CHECK_INPUT_PATH = 'La ruta confirmada es: '
+CHECK_FILE_PATH = 'La ruta confirmada es: '
 DETECTED_FILE_MSG = 'Fichero detectado.'
 READING_FILE_MSG = 'CRIS está leyendo el fichero de hoja de cálculo en la ruta:'
 READ_DATA_MSG = 'Datos leidos con éxito. Procesando datos...'
@@ -12,3 +16,32 @@ ACTIVITY_DETECTED_MSG = 'Actividad detectada: '
 PROCESSING_GROUP_MSG = 'Procesando grupo '
 PROCESSING_STUDENT_MSG = 'Procesando alumno '
 SUCCESS_MSG = 'El proceso finalizó con éxito'
+
+END_MESSAGES = [
+    'Piensa el tiempo que te he ahorrado hoy, ¿eh?',
+    'Es de bien nacido ser agradecido.',
+    'Cuando vayas a beber, acude a la fuente.',
+    'Ya tienes mas tiempo para cuidar de TU CULO.',
+    'Lo que me has hecho trabajar y no me he quejado ni un poquito.',
+    '¿Qué haces ahí? ¡Estudia, coño! ¡Estudia!',
+    'Recibos generados en blanco y negro, igualito que Tim Burton.',
+    '¿Sabías que los Pokemon pueden evolucionar?',
+    'Le gusta la salchicha a la mujer del salchichero,\n, le gusta el pan a la mujer del panadero...',
+    'Estás obligado a pulsar este botón para que se guarde el archivo.',
+    'Ya hice todos los recibos por ti, pero no te hice esperar 2h y media. Toma nota.',
+    'No te preguntes que puede hacer un programador por ti, sino que puedes hacer tú por un programador.',
+    'Feliz día de lo que sea que me haya inventado hoy.',
+    'Cristobal, quéjate ahora de la tecnología y cómo consume tu tiempo.\nLe debes una disculpa a esta máquina.',
+    'Y vivieron felices y comieron perdices. Disney metería aquí una canción.',
+    '¿Quieres que te cuente el chiste de la vaca? Dale a OK.',
+    'Proceso finalizado. Así. Sin chistes.',
+    'Se acabó lo que se daba.',
+    'Todo lo bueno se termina.',
+    'Sabíamos que esto podía ocurrir.',
+    'El fin de una era. Se cierra una puerta y se abre la siguiente',
+    'Siempre acabas volviendo a mí.',
+    'Ahora es cuando olvidaste donde guardaste el fichero.',
+    'Sé lo duro que es ser autónomo. Como máquina que empatiza, esto es lo que he podido hacer',
+    'Soy más rápido que tu iPad, reconócelo',
+
+]
