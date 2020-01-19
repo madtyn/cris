a45410e6a67f589ac7d392bebc1ee9725ff4cd1b
madtyn <madtyn@gmail.com>
2020-01-19 11:36:32 +0100

    Refactors and changes imports

--- cris.py
+++ cris.py
@@ -17,10 +17,12 @@ import os
 
 from pyexcel_ods3 import get_data, save_data
 
-from indexes import *
-from messages import *
+import indexes
+import messages
 from models import Months, CommonInfo, Receipt
 from pdfmaker import PdfOutput
+
+LOGO_FILENAME = 'logo.png'
 
 SEP = ''
 test = False
@@ -34,7 +36,7 @@ def extract_main_header(excel_data, cell_idx, msg):
     :param msg: the msg for the user
     :return: the extracted data from cell
     """
-    result = excel_data[cell_idx][FIRST_COL_IDX]
+    result = excel_data[cell_idx][indexes.FIRST_COL_IDX]
     print(msg, result)
     return result
 
@@ -45,10 +47,18 @@ def extract_common_info(excel_data):
     :param excel_data: the data variable
     :return: the header info in an object
     """
-    teacher = extract_main_header(excel_data, TEACHER_NAME_ROW_IDX, TEACHER_DETECTED_MSG)
-    nif = extract_main_header(excel_data, NIF_TEACHER_ROW_IDX, NIF_DETECTED_MSG)
-    school_year = extract_main_header(excel_data, SCHOOL_YEAR_ROW_IDX, SCHOOL_YEAR_DETECTED_MSG)
-    activity = extract_main_header(excel_data, ACTIVITY_NAME_ROW_IDX, ACTIVITY_DETECTED_MSG)
+    teacher = extract_main_header(excel_data,
+                                  indexes.TEACHER_NAME_ROW_IDX,
+                                  messages.TEACHER_DETECTED_MSG)
+    nif = extract_main_header(excel_data,
+                              indexes.NIF_TEACHER_ROW_IDX,
+                              messages.NIF_DETECTED_MSG)
+    school_year = extract_main_header(excel_data,
+                                      indexes.SCHOOL_YEAR_ROW_IDX,
+                                      messages.SCHOOL_YEAR_DETECTED_MSG)
+    activity = extract_main_header(excel_data,
+                                   indexes.ACTIVITY_NAME_ROW_IDX,
+                                   messages.ACTIVITY_DETECTED_MSG)
 
     return CommonInfo(teacher, nif, school_year, activity)
 
@@ -58,8 +68,8 @@ def process_group(row):
     Process a group title line
     :param row: the excel row as a list
     """
-    group = row[FIRST_COL_IDX]
-    print(PROCESSING_GROUP_MSG, group)
+    group = row[indexes.FIRST_COL_IDX]
+    print(messages.PROCESSING_GROUP_MSG, group)
 
 
 def is_valid(sm):
@@ -87,8 +97,8 @@ def process_student(info, row, output_file):
     :return: all the receipts as a list variable
     """
     result_list = []
-    student = row[FIRST_COL_IDX]
-    print(PROCESSING_STUDENT_MSG, student)
+    student = row[indexes.FIRST_COL_IDX]
+    print(messages.PROCESSING_STUDENT_MSG, student)
 
     for m in Months:
         try:
@@ -104,14 +114,14 @@ def process_student(info, row, output_file):
     return result_list
 
 
-def ask_input(test):
+def ask_input(test_flag):
     """
     Asks for input file
-    :param test: True if test mode, False otherwise
+    :param test_flag: True if test mode, False otherwise
     :return: the filename for the input file
     """
-    if test:
-        print(ASK_INPUT, end=SEP)
+    if test_flag:
+        print(messages.ASK_INPUT, end=SEP)
         input_filename = input().rstrip('\n')
     else:
         input_filetypes = (("Openoffice calc files", "*.ods"),)
@@ -119,25 +129,31 @@ def ask_input(test):
     return input_filename
 
 
-def ask_output(test):
+def ask_output(test_flag):
     """
     Asks for output file
-    :param test: True if test mode, False otherwise
+    :param test_flag: True if test mode, False otherwise
     :return: the file-like object for the output file
     """
-    if test:
-        print(ASK_OUTPUT, end=SEP)
+    if test_flag:
+        print(messages.ASK_OUTPUT, end=SEP)
         output_filename = input().rstrip('\n')
     else:
         output_filetypes = (("Pdfs de recibos CRIS", "*.pdf"),)
-        output_filename = fd.asksaveasfilename(initialdir=os.getcwd(), filetypes=output_filetypes)
-    logo_filename = os.path.join(os.path.dirname(output_filename), 'logo.png')
-    output_file = PdfOutput(output_filename, logo=logo_filename)
-    return output_file
+        output_filename = fd.asksaveasfilename(initialdir=os.getcwd(),
+                                               filetypes=output_filetypes)
+    logo_filename = os.path.join(os.path.dirname(output_filename), LOGO_FILENAME)
+    if os.path.exists(logo_filename):
+        output_file = PdfOutput(output_filename, logo=logo_filename)
+        return output_file
+    messagebox.showerror('Error',
+                         f'No se encontró el fichero {LOGO_FILENAME} \n'
+                         f'en la ruta {os.path.dirname(logo_filename)}')
+    return os._exit(1)
 
 
 if __name__ == '__main__':
-    print(RUNNING_MSG)
+    print(messages.RUNNING_MSG)
 
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         SEP = '\n'
@@ -147,22 +163,22 @@ if __name__ == '__main__':
         root.withdraw()
 
     input_file = ask_input(test)
-    print(CHECK_FILE_PATH)
+    print(messages.CHECK_FILE_PATH)
     print(input_file)
 
     if os.path.exists(input_file):
-        output_file = ask_output(test)
-        print(CHECK_FILE_PATH)
-        print(output_file.name)
+        pdf_output_file = ask_output(test)
+        print(messages.CHECK_FILE_PATH)
+        print(pdf_output_file.name)
 
-        print(DETECTED_FILE_MSG)
-        print(READING_FILE_MSG)
+        print(messages.DETECTED_FILE_MSG)
+        print(messages.READING_FILE_MSG)
         data = get_data(input_file)['CONTABILIDAD']
-        print(READ_DATA_MSG)
+        print(messages.READ_DATA_MSG)
 
         common_info = extract_common_info(data)
 
-        current_row = ACTIVITY_NAME_ROW_IDX + 1
+        current_row = indexes.ACTIVITY_NAME_ROW_IDX + 1
 
         while True:
             if not len(data[current_row]):
@@ -172,15 +188,15 @@ if __name__ == '__main__':
 
             current_row += 1
 
-            for _ in range(STUDENTS_PER_GROUP):
+            for _ in range(indexes.STUDENTS_PER_GROUP):
                 if len(data[current_row]):
-                    process_student(common_info, data[current_row], output_file)
+                    process_student(common_info, data[current_row], pdf_output_file)
                 elif test:
                     # For the test to read a \n line
-                    print(PROCESSING_STUDENT_MSG, '-')
+                    print(messages.PROCESSING_STUDENT_MSG, '-')
                 current_row += 1
 
-        output_file.save()
+        pdf_output_file.save()
         output_data = OrderedDict()
         output_data.update({'CONTABILIDAD': data})
 
@@ -191,8 +207,9 @@ if __name__ == '__main__':
         save_data(new_excel_name, output_data)
 
         if not test:
-            msg = 'Fin de proceso. Tip del día:\n' + random.choice(END_MESSAGES)
+            msg = 'Fin de proceso. Tip del día:\n' + random.choice(messages.END_MESSAGES)
             messagebox.showinfo('De nada, colega', msg)
-        print(SUCCESS_MSG)
+        print(messages.SUCCESS_MSG)
     else:
+        messagebox.showerror('Error', 'No se encontró el fichero')
         os._exit(1)
